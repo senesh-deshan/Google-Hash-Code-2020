8257c7571b034b62c616c6bd2c9fb5941ef2dc21
Ramon Medeiros <ramon.medeiros@recordunion.com>
2020-02-20 01:31:23 +0100

    Small changes on the Solution
    - Use less variables (and memory)
    - Reduce ifs

--- Solution/Solution.py
+++ Solution/Solution.py
@@ -7,6 +7,8 @@
 #  $$    $$ $$       $$   $$$ $$       $$    $$ $$     $$    $$     $$ $$       $$    $$ $$     $$ $$     $$ $$   $$$ #
 #   $$$$$$  $$$$$$$$ $$    $$ $$$$$$$$  $$$$$$  $$     $$    $$$$$$$$  $$$$$$$$  $$$$$$  $$     $$ $$     $$ $$    $$ #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+import os
+import sys
 
 # More Pizza
 # Solution for the Practice Round of Google Hash Code 2020
@@ -14,77 +16,51 @@
 # https:#github.com/senesh-deshan/Google-Hash-Code-2020
 
 
-def solve(MAX, inputList):
+def solve(target, inputList, length):
 
-    solutionIndexList = [] # To store the best solution indexes through out the solution generating
-    solutionValueList = [] # To store the best solution values through out the solution generating
-    currentIndexList = [] # To store the current solution indexes through out the solution generating
-    currentValueList = [] # To store the current solution values through out the solution generating
-
-    fullSize = len(inputList)
-
+    current = [] # To store the current solution indexes through out the solution generating
     maxScore = 0 # Stores the maximum score achieved through out the solution generating
-
-    startIndex = fullSize # Stores index which the solution generating should start from
-
     sum = 0
 
     # Solution generating starts from the last element of the inputList
-    # If first element of currentIndexList becomes 0 that means solution generating is finished
+    # If first element of current becomes 0 that means solution generating is finished
 
-    while((len(currentIndexList) > 0 and currentIndexList[0] != 0) or len(currentIndexList) == 0):
+    while((len(current) > 0 and current[0] != 0) or len(current) == 0):
 
-        startIndex = startIndex - 1 
+        length = length - 1 
 
-        for i in range(startIndex, -1, -1): # Used to traverse from end to start of the inputList
+        # Used to traverse from end to start of the inputList
+        for i in range(length, -1, -1):
 
             currentValue = inputList[i]
 
             tempSum = sum + currentValue 
 
-            if (tempSum == MAX):  # If the temporary sum is equal to target that means the perfect solution is found
+            if tempSum <= target:
                 sum = tempSum
-                currentIndexList.append(i) # Add current Pizza index to the solution
-                currentValueList.append(currentValue) # Add current Pizza value to the solution
-                break  # Go to return solution
+                current.append(i) # Add current Pizza index to the solution
 
-            elif (tempSum > MAX):  # If the temporary sum is greter than target
-                continue  # Try next value
+                # If the temporary sum is equal to target that means the perfect solution is found
+                if (tempSum == target):
+                    return current, sum
 
-            elif (tempSum < MAX):  # If the temporary sum is lesser than target
-                sum = tempSum
-                currentIndexList.append(i) # Add current Pizza index to the solution
-                currentValueList.append(currentValue) # Add current Pizza value to the solution
-                continue  # Try next value
-
-        if (maxScore < sum):  # If currently generated solution has the best score
+        # If currently generated solution has the best score
+        if (maxScore < sum):  
             maxScore = sum  # Save its value
+            solution = current.copy()
 
-            solutionIndexList = []
-            solutionValueList = []
+        # remove smaller number and try even smaller ones
+        if(len(current) != 0):
+            lastIndex = current.pop()
+            sum = sum - inputList[lastIndex]
+            length = lastIndex # Make it as the starting index for the next iteration
 
-            for y in currentIndexList:
-                solutionIndexList.append(y)  # Save the currently best solution indexes
-            for y in currentValueList:
-                solutionValueList.append(y)  # Save the currently best solution values
-
-        if (maxScore == MAX):  # If current solution is the perfect solution
+        # If solution generating is almost finished
+        if(len(current) == 0 and (length == 0)): 
             break # Stop solution generating
 
-        if(len(currentValueList) != 0):
-            lastVal = currentValueList.pop() # Remove the last element from current values
-            sum = sum - lastVal # Subtract it from sum
-
-        if(len(currentIndexList) != 0):
-            lastIndex = currentIndexList.pop() # Remove the last element from current indexes
-            startIndex = lastIndex # Make it as the starting index for the next iteration
-
-        if(len(currentIndexList) == 0 and (startIndex == 0)): # If solution generating is almost finished
-            break # Stop solution generating
-
-    print("SCORE = " + str(maxScore))     # Print the score of the best solution
-
-    return solutionIndexList  # Return indexes list of the best solution
+    # Return indexes list of the best solution
+    return solution, maxScore
 
 
 def process(fileName):
@@ -115,7 +91,7 @@ def process(fileName):
     #  Create the pizza list by reading the file
     inputList = list(map(int, secondLine.split()))
 
-    outputList = solve(MAX, inputList)  # Solve the problem and get output
+    outputList, sum = solve(MAX, inputList, NUM)  # Solve the problem and get output
 
     #  Print output data and create output file
        
